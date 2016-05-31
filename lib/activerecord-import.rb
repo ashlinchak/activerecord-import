9f6d0ecc2e34761a3f65a821bb023960ffc37e0a
Alexander <ashlinchak@gmail.com>
2016-05-31 10:58:12 +0300

    return

--- lib/activerecord-import.rb
+++ lib/activerecord-import.rb
@@ -1,16 +1,18 @@
 # rubocop:disable Style/FileName
 
-class ActiveRecord::Base
-  class << self
-    def establish_connection_with_activerecord_import(*args)
-      conn = establish_connection_without_activerecord_import(*args)
-      if !ActiveRecord.const_defined?(:Import) || !ActiveRecord::Import.respond_to?(:load_from_connection_pool)
-        require "activerecord-import/base"
-      end
+ActiveSupport.on_load(:active_record) do
+  class ActiveRecord::Base
+    class << self
+      def establish_connection_with_activerecord_import(*args)
+        conn = establish_connection_without_activerecord_import(*args)
+        if !ActiveRecord.const_defined?(:Import) || !ActiveRecord::Import.respond_to?(:load_from_connection_pool)
+          require "activerecord-import/base"
+        end
 
-      ActiveRecord::Import.load_from_connection_pool connection_pool
-      conn
+        ActiveRecord::Import.load_from_connection_pool connection_pool
+        conn
+      end
+      alias_method_chain :establish_connection, :activerecord_import
     end
-    alias_method_chain :establish_connection, :activerecord_import
   end
 end
